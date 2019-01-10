54c4029d70869fb3fe978ff37c33e31cf1fd08be
BalramMitta <balus211221@gmail.com>
2019-01-10 16:30:26 +0530

    #incremental assignment 3 question 5

---
+++ assignmentsNew/src/main/java/jia2/cycleFactory/Factory.java
@@ -0,0 +1,97 @@
+package jia2.cycleFactory;
+
+interface Cycle{
+    void ride();
+}
+
+interface CycleFactory{
+    Cycle getCycle();
+}
+
+class Unicycle implements Cycle{
+
+    @Override
+    public void ride() {
+        System.out.println("Riding Unicycle");
+    }
+}
+
+class Bicycle implements Cycle{
+
+    @Override
+    public void ride() {
+        System.out.println("Riding bicycle");
+    }
+}
+
+class Tricycle implements Cycle{
+
+    @Override
+    public void ride() {
+        System.out.println("Riding tricycle");
+    }
+
+}
+
+class UnicycleFactory implements CycleFactory{
+
+    @Override
+    public Cycle getCycle() {
+        return new Unicycle();
+    }
+}
+
+class BicycleFactory implements CycleFactory{
+
+    @Override
+    public Cycle getCycle() {
+        return new Bicycle();
+    }
+}
+class TricycleFactory implements CycleFactory{
+
+    @Override
+    public Cycle getCycle() {
+        return new Tricycle();
+    }
+}
+
+public class Factory {
+
+    Cycle getCycle(String cycleName){
+
+        CycleFactory cycleFactory;
+        if("Uni".equalsIgnoreCase(cycleName)){
+            cycleFactory = new UnicycleFactory();
+        }
+        else if("Bi".equalsIgnoreCase(cycleName)){
+            cycleFactory = new BicycleFactory();
+        }
+        else if("Tri".equalsIgnoreCase(cycleName)){
+            cycleFactory = new TricycleFactory();
+        }
+        else {
+            System.out.println("No mentioned Cycle");
+            return null;
+        }
+
+        return cycleFactory.getCycle();
+    }
+
+    public static void main(String[] args){
+
+        Factory factory = new Factory();
+
+        Cycle cycle1 = factory.getCycle("uni");
+
+        Cycle cycle2 = factory.getCycle("bi");
+
+        Cycle cycle3 = factory.getCycle("tri");
+
+        cycle1.ride();
+        cycle2.ride();
+        cycle3.ride();
+
+    }
+
+}
---
+++ assignmentsNew/src/main/java/jia2/q5/InnerClass.java
@@ -0,0 +1,38 @@
+package jia2.q5;
+
+
+
+class A{
+
+    class InnerA{
+
+        InnerA(String s){
+            System.out.println("Inside Inner A \n "+s);
+        }
+
+    }
+}
+
+class B{
+
+    class InnerB extends A.InnerA{
+
+        InnerB(A a,String s) {
+            a.super(s);
+        }
+
+    }
+
+}
+
+public class InnerClass {
+
+    public static void main(String[] args){
+
+        A a=new A();
+        B b=new B();
+        B.InnerB innerB=b.new InnerB(a,"Hello Balram");
+
+    }
+
+}
