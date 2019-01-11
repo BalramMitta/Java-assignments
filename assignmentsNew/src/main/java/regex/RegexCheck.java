997586a289c81d5ce9d269f6d4b0d9092f5ed1db
BalramMitta <balus211221@gmail.com>
2019-01-11 10:55:12 +0530

    #incremental assignment 5

---
+++ assignmentsNew/src/main/java/regex/RegexCheck.java
@@ -0,0 +1,35 @@
+package regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RegexCheck {
+
+    public static void main(String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+
+        int f;
+
+        System.out.println("Enter a sentence");
+
+        String sentence = scanner.nextLine();   // taking input from command line.
+
+        String matchingString = "^[A-Z].*[\\.]$";   // Regular expression for a string starting with capital letter and ending with a dot(.).
+
+        /*
+        Pattern.matches takes first argument regex and second argument string
+
+        returns boolean value
+
+        true if regex matches with string else false
+        */
+
+        if(Pattern.matches(matchingString,sentence))
+            System.out.println("String matched");
+        else
+            System.out.println("Not matched with regular expression");
+
+    }
+
+}
