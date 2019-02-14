820e276e5eabcf4e357f240fc655fb8d6dee47e4
BalramMitta <balus211221@gmail.com>
2019-02-14 12:32:06 +0530

    Reformatted

--- assignmentsNew/src/main/java/jia7/CharacterCount.java
+++ assignmentsNew/src/main/java/jia7/CharacterCount.java
@@ -4,56 +4,84 @@ package jia7;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class CharacterCount {
+
+    private static final Logger LOG = Logger.getLogger(CharacterCount.class.getName());
+    private static final File OUTPUT_FILE = new File("assignmentsNew/src/main/java/jia7/OUTPUT_FILE.txt");
+
+    /**
+     * Reads the content of the file looping through each line and each character
+     * Storing character as key and its count as value in map
+     *
+     * @param filePath input file path
+     * @return Character Count HashMap
+     * @throws IOException
+     */
+    private static Map<Character, Integer> getCharacterCount(String filePath) throws IOException {
+        File inputFile = new File(filePath);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+
+        Map<Character, Integer> characterCountHashMap = new HashMap<>();
+
+        String line;
+        char key;
+
+        while ((line = bufferedReader.readLine()) != null) {
+
+            for (int i = 0; i < line.length(); i++) {
+
+                key = line.charAt(i);
+
+                // if character exists already than increment the count in value else put 1.
+                if (characterCountHashMap.containsKey(key))
+                    characterCountHashMap.put(key, characterCountHashMap.get(key) + 1);
+                else
+                    characterCountHashMap.put(key, 1);
+            }
+        }
+        bufferedReader.close();
+
+        return characterCountHashMap;
+    }
+
+
+    /**
+     * Writes character and its in count in each line in output file
+     * @param characterCountHashMap map containing key value as character and its count in input file
+     * @throws IOException
+     */
+    private static void writeCharacterCountToFile(Map<Character, Integer> characterCountHashMap) throws IOException {
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE));
+
+        // writing each character and its occurrence count in OUTPUT_FILE file
+        for (Map.Entry<Character, Integer> m : characterCountHashMap.entrySet()) {
+
+            bufferedWriter.write(m.getKey() + " : " + m.getValue());
+            bufferedWriter.newLine();
+
+        }
+
+        LOG.info("Results written to output.txt file");
+
+        bufferedWriter.close();
+    }
+
 
     public static void main(String[] args) {
 
         try {
 
-            String path = args[0];                                                      // getting path to the file from command line argument
-            File input = new File(path);
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(input));  // Using buffered reader to read the contents of file
+            String path = args[0];
 
-            Map<Character, Integer> map = new HashMap<>();
+            Map<Character, Integer> characterCountHashMap = getCharacterCount(path);
 
-            String line;
-            char key;
-
-            // reading each line from the contents of file
-            while ((line = bufferedReader.readLine()) != null) {
-
-                // reading each character from the line
-                for (int i = 0; i < line.length(); i++) {
-
-                    // storing each character as key
-                    key = line.charAt(i);
-
-                    // if character exists already than increment the count in value else put 1.
-                    if (map.containsKey(key))
-                        map.put(key, map.get(key) + 1);
-                    else
-                        map.put(key, 1);
-                }
-            }
-            File output = new File("assignmentsNew/src/main/java/jia7/output.txt");
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output));
-
-            // writing each character and its occurrence count in output file
-            for (Map.Entry<Character, Integer> m : map.entrySet()) {
-
-                bufferedWriter.write(m.getKey() + " : " + m.getValue());
-                bufferedWriter.newLine();
-
-            }
-
-            System.out.println("Results written to output.txt file");
-
-            bufferedReader.close();
-            bufferedWriter.close();
+            writeCharacterCountToFile(characterCountHashMap);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.info(e.getMessage());
         }
 
 
